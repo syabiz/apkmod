2e47c0c2502e965033b34a16fdd505b99d0d4df1
Saipul Bakri <35644533+syabiz@users.noreply.github.com>
2022-11-14 03:19:12 +0700

    Update aRel.js

--- js/aRel.js
+++ js/aRel.js
@@ -1,3 +1,80 @@
 /*<![CDATA[*/
-function _0x367e(){var a=["6750477VoDyyj","includes","?alt=json-in-script&orderby=updated&start-index=","href","relOuter","2374218itFFYH","78UsTXwu","&amp;","newTabLink","3047476vqZcuk",")</span>","link","toString","src",'" href="',"match","parentNode","38RvZbNm","openSearch$totalResults",'</span> <span class="m">(',"&max-results=","97801AmrCOR","/feeds/posts/summary","replace","numbPs","innerHTML","floor","</ol>","</div></a></li>","&callback=autoRelatedPosts","%26","title","length","30bwiYNm","_Games",' aria-label="',"<li><a","undefined","?alt=json-in-script&orderby=updated&max-results=0&callback=autoRandomIndex","10724vLZLZo","/-/","appendChild",' target="_blank"',"homePgUrl","2135434jjxEcA","aRel",'<ol class="autoRpst">',"_Apps",' <span class="v">v',"feed","rel","8vTrgnH","postUrl","/feeds/posts/default","head",'<ol class="rPst-ul autoRpst">',"entry","alternate","</span>","object","content","remove","createElement","63185FZJPJp","script",'"> <div class="itmTtl"><span>',"splice","getElementsByTagName","random"];return(_0x367e=function(){return a})()}function _0x2668(a,e){var s=_0x367e();return(_0x2668=function(a,e){return s[a-=100]})(a,e)}var _0x2e3fa3=_0x2668;(function(a,e){for(var s=_0x2668,t=_0x367e();;)try{if(487330===-parseInt(s(118))/1*(-parseInt(s(140))/2)+parseInt(s(106))/3+-parseInt(s(110))/4+-parseInt(s(164))/5*(parseInt(s(107))/6)+-parseInt(s(145))/7*(parseInt(s(152))/8)+parseInt(s(101))/9+-parseInt(s(134))/10*(parseInt(s(122))/11))break;t.push(t.shift())}catch(a){t.push(t.shift())}})(),_0x2e3fa3(138)!=typeof xAo&&function(){var a,e,s=_0x2e3fa3,t={numbPs:7,newTabLink:!0,relOuter:s(146)};for(var n in rPstConfig)s(138)!=rPstConfig[n]&&(t[n]=rPstConfig[n]);function r(a){var e=s,t=document[e(163)](e(165));t[e(114)]=a,document[e(168)](e(155))[0][e(142)](t)}function l(a){var e,t,n=s,r=a[n(133)];if(0===r)return!1;for(;--r;)e=Math[n(127)](Math[n(100)]()*(r+1)),t=a[r],a[r]=a[e],a[e]=t;return a}if(a=null==autoRelatedPost[s(125)]?t[s(125)]:autoRelatedPost[s(125)],e=autoRelatedPost[s(109)]?s(143):"",s(160)==typeof psLabels&&0<psLabels[s(133)])var o=s(141)+l(psLabels)[0][s(124)](s(108),s(131));else o="";autoRelatedPosts=function(a){var n,r,o,p,i,u=s,f="",c=getid(t[u(105)]),d=l(a[u(150)][u(157)]);if(psLabels[u(102)](u(148))||psLabels[u(102)](u(135)))var v=u(147);else v=u(156);f+=v;for(var m=0;m<d[u(133)];m++)for(var b=0,x=d[m][u(112)][u(133)];b<x;b++)d[m][u(112)][b][u(104)]==t[u(153)]&&d[u(167)](m,1);if(0<d[u(133)]){for(var g=0;g<d[u(133)];g++){if(r=d[g][u(132)].$t,o=u(161)in d[g]&&(o=d[g][u(161)].$t[u(116)](/<span.class=.apName.[\s\S]*?<\/span>/g))||"",p=u(161)in d[g]&&(p=d[g][u(161)].$t[u(116)](/<span.class=.apVersi.[\s\S]*?<\/span>/g))||"",i=u(161)in d[g]&&(i=d[g][u(161)].$t[u(116)](/<span.class=.apMod.[\s\S]*?<\/span>/g))||"",""!=p)var h=p[0][u(116)](/(?<=<span.*?class="apVersi".*?>)(.*?)(?=<\/span>)/g);else h="";if(""!=i)var _=i[0][u(116)](/(?<=<span.* ?class="apMod".*?>)(.*?)(?=<\/span>)/g);else _="";if(""!=h)if(""!=_)var I=u(149)+h+u(120)+_+u(111);else I=u(149)+h+u(159);else I="";var P=0;for(x=d[g][u(112)][u(133)];P<x;P++)n=u(158)==d[g][u(112)][P][u(151)]?d[g][u(112)][P][u(104)]:"#";var R=r;""!==o&&(r=o[u(113)]()[u(124)](/<(.|\n)*?>/,"")),f+=u(137)+e+u(136)+R+u(115)+n+u(166)+r+u(159)+I+u(129)}c[u(126)]=f+=u(128)}else getid(u(146))[u(117)][u(162)]()},autoRandomIndex=function(e){var n=s,l=e[n(150)][n(119)].$t-7,p=1<l?1+Math[n(127)](Math[n(100)]()*l):1;r(t[n(144)][n(124)](/\/$/,"")+n(154)+o+n(103)+p+n(121)+a+n(130))},r(t[s(144)][s(124)](/\/$/,"")+s(123)+o+s(139))}();
-/*]]>*/
+function _0x367e() {
+	var a = ["6750477VoDyyj", "includes", "?alt=json-in-script&orderby=updated&start-index=", "href", "relOuter", "2374218itFFYH", "78UsTXwu", "&amp;", "newTabLink", "3047476vqZcuk", ")</span>", "link", "toString", "src", '" href="', "match", "parentNode", "38RvZbNm", "openSearch$totalResults", '</span> <span class="m">(', "&max-results=", "97801AmrCOR", "/feeds/posts/summary", "replace", "numbPs", "innerHTML", "floor", "</ol>", "</div></a></li>", "&callback=autoRelatedPosts", "%26", "title", "length", "30bwiYNm", "_Games", ' aria-label="', "<li><a", "undefined", "?alt=json-in-script&orderby=updated&max-results=0&callback=autoRandomIndex", "10724vLZLZo", "/-/", "appendChild", ' target="_blank"', "homePgUrl", "2135434jjxEcA", "aRel", '<ol class="autoRpst">', "_Apps", ' <span class="v">v', "feed", "rel", "8vTrgnH", "postUrl", "/feeds/posts/default", "head", '<ol class="rPst-ul autoRpst">', "entry", "alternate", "</span>", "object", "content", "remove", "createElement", "63185FZJPJp", "script", '"> <div class="itmTtl"><span>', "splice", "getElementsByTagName", "random"];
+	return (_0x367e = function() {
+		return a
+	})()
+}
+
+function _0x2668(a, e) {
+	var s = _0x367e();
+	return (_0x2668 = function(a, e) {
+		return s[a -= 100]
+	})(a, e)
+}
+var _0x2e3fa3 = _0x2668;
+(function(a, e) {
+	for (var s = _0x2668, t = _0x367e();;) try {
+		if (487330 === -parseInt(s(118)) / 1 * (-parseInt(s(140)) / 2) + parseInt(s(106)) / 3 + -parseInt(s(110)) / 4 + -parseInt(s(164)) / 5 * (parseInt(s(107)) / 6) + -parseInt(s(145)) / 7 * (parseInt(s(152)) / 8) + parseInt(s(101)) / 9 + -parseInt(s(134)) / 10 * (parseInt(s(122)) / 11)) break;
+		t.push(t.shift())
+	} catch (a) {
+		t.push(t.shift())
+	}
+})(), _0x2e3fa3(138) != typeof xAo && function() {
+	var a, e, s = _0x2e3fa3,
+		t = {
+			numbPs: 7,
+			newTabLink: !0,
+			relOuter: s(146)
+		};
+	for (var n in rPstConfig) s(138) != rPstConfig[n] && (t[n] = rPstConfig[n]);
+
+	function r(a) {
+		var e = s,
+			t = document[e(163)](e(165));
+		t[e(114)] = a, document[e(168)](e(155))[0][e(142)](t)
+	}
+
+	function l(a) {
+		var e, t, n = s,
+			r = a[n(133)];
+		if (0 === r) return !1;
+		for (; --r;) e = Math[n(127)](Math[n(100)]() * (r + 1)), t = a[r], a[r] = a[e], a[e] = t;
+		return a
+	}
+	if (a = null == autoRelatedPost[s(125)] ? t[s(125)] : autoRelatedPost[s(125)], e = autoRelatedPost[s(109)] ? s(143) : "", s(160) == typeof psLabels && 0 < psLabels[s(133)]) var o = s(141) + l(psLabels)[0][s(124)](s(108), s(131));
+	else o = "";
+	autoRelatedPosts = function(a) {
+		var n, r, o, p, i, u = s,
+			f = "",
+			c = getid(t[u(105)]),
+			d = l(a[u(150)][u(157)]);
+		if (psLabels[u(102)](u(148)) || psLabels[u(102)](u(135))) var v = u(147);
+		else v = u(156);
+		f += v;
+		for (var m = 0; m < d[u(133)]; m++)
+			for (var b = 0, x = d[m][u(112)][u(133)]; b < x; b++) d[m][u(112)][b][u(104)] == t[u(153)] && d[u(167)](m, 1);
+		if (0 < d[u(133)]) {
+			for (var g = 0; g < d[u(133)]; g++) {
+				if (r = d[g][u(132)].$t, o = u(161) in d[g] && (o = d[g][u(161)].$t[u(116)](/<span.class=.apName.[\s\S]*?<\/span>/g)) || "", p = u(161) in d[g] && (p = d[g][u(161)].$t[u(116)](/<span.class=.apVersi.[\s\S]*?<\/span>/g)) || "", i = u(161) in d[g] && (i = d[g][u(161)].$t[u(116)](/<span.class=.apMod.[\s\S]*?<\/span>/g)) || "", "" != p) var h = p[0][u(116)](/(?<=<span.*?class="apVersi".*?>)(.*?)(?=<\/span>)/g);
+				else h = "";
+				if ("" != i) var _ = i[0][u(116)](/(?<=<span.* ?class="apMod".*?>)(.*?)(?=<\/span>)/g);
+				else _ = "";
+				if ("" != h)
+					if ("" != _) var I = u(149) + h + u(120) + _ + u(111);
+					else I = u(149) + h + u(159);
+				else I = "";
+				var P = 0;
+				for (x = d[g][u(112)][u(133)]; P < x; P++) n = u(158) == d[g][u(112)][P][u(151)] ? d[g][u(112)][P][u(104)] : "#";
+				var R = r;
+				"" !== o && (r = o[u(113)]()[u(124)](/<(.|\n)*?>/, "")), f += u(137) + e + u(136) + R + u(115) + n + u(166) + r + u(159) + I + u(129)
+			}
+			c[u(126)] = f += u(128)
+		} else getid(u(146))[u(117)][u(162)]()
+	}, autoRandomIndex = function(e) {
+		var n = s,
+			l = e[n(150)][n(119)].$t - 7,
+			p = 1 < l ? 1 + Math[n(127)](Math[n(100)]() * l) : 1;
+		r(t[n(144)][n(124)](/\/$/, "") + n(154) + o + n(103) + p + n(121) + a + n(130))
+	}, r(t[s(144)][s(124)](/\/$/, "") + s(123) + o + s(139))
+}();/*]]>*/
